03ff3821302134fe3309d19fb033eefbc946e7db
Monosix8 <ridhohidayatullah008@gmail.com>
2025-12-05 22:41:51 +0700

    theme

--- app/src/main/java/com/example/roomdatabase/ui/theme/Theme.kt
+++ app/src/main/java/com/example/roomdatabase/ui/theme/Theme.kt
@@ -1,6 +1,3 @@
-package com.example.roomdatabase.ui.theme
-
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -10,6 +7,12 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.example.myroomsatu.ui.theme.Pink40
+import com.example.myroomsatu.ui.theme.Pink80
+import com.example.myroomsatu.ui.theme.Purple40
+import com.example.myroomsatu.ui.theme.Purple80
+import com.example.myroomsatu.ui.theme.PurpleGrey40
+import com.example.myroomsatu.ui.theme.PurpleGrey80
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -34,7 +37,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun RoomDataBaseTheme(
+fun MyRoomSatuTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
@@ -52,7 +55,6 @@ fun RoomDataBaseTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
         content = content
     )
 }
