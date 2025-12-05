00c5d7e43297d6bb2cf9d826e6c0fdbc41db6fd8
Monosix8 <ridhohidayatullah008@gmail.com>
2025-12-05 22:43:06 +0700

    main

--- app/src/main/java/com/example/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabase/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.roomdatabase
+package com.example.myroomsatu
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,17 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.roomdatabase.ui.theme.RoomDataBaseTheme
+import com.example.myroomsatu.ui.theme.MyRoomSatuTheme
+import com.example.roomdatabase.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RoomDataBaseTheme {
+            MyRoomSatuTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    RoomDataBaseTheme {
+    MyRoomSatuTheme {
         Greeting("Android")
     }
 }
