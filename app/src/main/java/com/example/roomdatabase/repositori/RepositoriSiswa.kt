0c79368537b6875810bc5e17f0cc49d6887a5c7f
Monosix8 <ridhohidayatullah008@gmail.com>
2025-12-05 22:42:29 +0700

    repo siswa

---
+++ app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
@@ -0,0 +1,28 @@
+package com.example.myroomsatu.repositori
+
+import com.example.myroomsatu.room.Siswa
+import com.example.myroomsatu.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+
+    suspend fun deleteSiswa(siswa: Siswa)
+
+    suspend fun updateSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
+}
